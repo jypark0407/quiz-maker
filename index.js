57d1f8621f75557dfde5e607cf45d7cc12e5d204
Jinyoung Park <jypark04070@gmail.com>
2019-11-09 12:55:52 +0900

    Clean code

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+window.addEventListener("click", clicked)
+
+const score_list  = [];
+
 function clicked (e) {
     const parentContainer = e.target.parentNode.parentNode.parentNode;
     const containerId= Number(parentContainer.id);
@@ -6,62 +10,61 @@ function clicked (e) {
     const classAnswerList= [...classAnswer.parentNode.children];
 
     const next = document.getElementById(String(containerId+1));
-    
-    function applyChange() {
-        classAnswer.classList.add("clicked");
-        const unclickedElements = classAnswerList.filter(element => !element.classList.contains("clicked"))
-        unclickedElements.map(element => element.classList.add("unclicked"));
-    }
-    
-    function scrollNext() {
-       next.scrollIntoView({behavior:"smooth"});
-    }
-    
-    function addScore() {
-        const score = Number(classAnswer.getAttribute("data-score"));
-        score_list.push(score);
-        console.log(score_list)
-    }
-    const sum = (initialValue, currentValue) => initialValue + currentValue;
-    
-    function calculate() {
-        return (score_list.reduce(sum, 0))
-    }
-    
-    function displayResult() {
-        if (calculatedScore >0 || calculateScore < 15) {
-            var i = 1;
-        }
-        if (calculatedScore >= 15 || calculatedScore < 30) {
-            var i = 2;
-        }
-        if (calculatedScore >=30 || calculatedScore < 40) {
-            var i = 3;
-        } else {
-            var i = 4;
-        }
-        const resultList = Array.from(document.querySelectorAll(".result"));
-        console.log(resultList)
-        const result = (resultList.filter(element=> element.id == i.toString()))[0]
-        console.log(result)
-        result.classList.add("result-clicked");
-        console.log(result.classList.add("result-clicked"))
-    }   
 
-    const calculatedScore = calculate();
+    applyChange(classAnswerList, classAnswer);
 
-    if (score_list.length == 4) {
-        applyChange();
-        calculate();
-        displayResult();
+    if (score_list.length === 4) {
+        displayResult(calculate(score_list));
     } else {
-        applyChange();
-        scrollNext();
-        addScore();
+        addScore(classAnswer, score_list);
+        scrollNext(next)
     }
 }
 
-window.addEventListener("click", clicked)
+function applyChange(allAnswers, clickedAnswer  ) {
+    allAnswers.forEach(element => {
+        element.classList.remove("clicked");
+        element.classList.remove("unclicked");
+    })
+    clickedAnswer.classList.add("clicked");
+    allAnswers.forEach(element => {
+        if(!element.classList.contains("clicked")) {
+            element.classList.add("unclicked");
+        }
+    })
+}
 
-const score_list = [];
+function addScore(clickedAnswer, score_list) {
+    const score = Number(clickedAnswer.getAttribute("data-score"));
+    score_list.push(score);
+    console.log(score_list)
+}
 
+function scrollNext(nextContainer) {
+    nextContainer.scrollIntoView({behavior:"smooth"});
+}
+
+function calculate(scores) {
+    return scores.reduce((init, cur) => init + cur)
+}
+
+function displayResult(calculatedScore) {
+    if (calculatedScore >0 || calculateScore < 15) {
+        var i = 1;
+    }
+    if (calculatedScore >= 15 || calculatedScore < 30) {
+        var i = 2;
+    }
+    if (calculatedScore >=30 || calculatedScore < 40) {
+        var i = 3;
+    } else {
+        var i = 4;
+    }
+    const resultList = Array.from(document.querySelectorAll(".result"));
+    // console.log(resultList)
+    const result = (resultList.filter(element=> element.id == i.toString()))[0]
+    // console.log(result)
+    result.classList.add("result-clicked");
+    scrollNext(result);
+    // console.log(result.classList.add("result-clicked"))
+} 
